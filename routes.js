40b846eaa7c6dce1c14c638e076b2f45396bd28d
notskamr <66314478+notskamr@users.noreply.github.com>
2023-04-09 17:44:26 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 // routes is an array of name, url objects that is displayed on the site for testing
 const routes = [{
   name: "Firewood (In Progress Testing)",
-  url: "https://82cb-122-162-145-115.ngrok-free.app"
+  url: "miro.serveo.net"
 }]
